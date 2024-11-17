def4e7358b0e16c7c48a20c77a2eab8f97d2f8d8
danil <rush09-09@mail.ru>
2024-11-17 18:07:49 +0300

    Теперь каждое изображение открывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -4,10 +4,6 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from bottle import response
-from pygame.display import update
-from pygame.examples.cursors import image
-
 
 def load_image(url):
     try:
@@ -22,22 +18,25 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
 
 
 def exit():
     window.destroy()
 
+
 window = Tk()
 window.title('Cats')
 window.geometry('600x520')
 
-label = Label()
-label.pack()
 
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
@@ -47,7 +46,7 @@ window.config(menu= menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='Файл', menu=file_menu)
-file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_command(label='Загрузить фото', command=open_new_window())
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
